b93fc022017f94b29490ae35f39811951c502cb8
Shun Yuan <tanshunyuan99@gmail.com>
2024-10-19 19:55:24 +0800

    feat: add fn to create expense

--- src/pages/expenses/create.tsx
+++ src/pages/expenses/create.tsx
@@ -11,10 +11,12 @@ import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover
 import { cn } from "~/lib/utils"
 import { Calendar } from "~/components/ui/calendar"
 import { Link } from "@tanstack/react-router"
+import { supabase } from "~/utils/client"
+import { useState } from "react"
 const formSchema = z.object({
   incurredDate: z.date(),
-  from: z.string(),
-  to: z.string(),
+  from: z.string().min(3),
+  to: z.string().min(3),
   amount: z.coerce.number(),
   status: z.enum(['backlog', 'processing', 'reimbursed']),
   attachment: z
@@ -36,15 +38,62 @@ export const CreateExpensePage = () => {
     }
   })
   const attachmentRef = form.register("attachment");
+  const [uploading, setUploading] = useState(false)
 
-  const onSubmit = (data: FormSchema) => {
-    console.log(data)
+  const handleAttachmentUpload = async (item: FileList) => {
+    try {
+      setUploading(true)
+
+      const file = item[0]
+      const fileExt = file.name.split('.').pop()
+      const fileName = `${Math.random()}.${fileExt}`
+      const filePath = `${fileName}`
+
+      const { error: uploadError, data } = await supabase.storage.from('expenses-media').upload(filePath, file)
+
+      if (uploadError) {
+        throw uploadError
+      }
+
+      return data.fullPath
+
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setUploading(false)
+    }
+  }
+
+  const onSubmit = async (data: FormSchema) => {
+    console.log(data.attachment)
+
+    try {
+      const imagePath = await handleAttachmentUpload(data.attachment)
+      const uploadUrl = await supabase
+        .storage
+        .from('expenses-media')
+        .getPublicUrl(imagePath)
+
+      await supabase.from('expenses').insert({
+        amount: data.amount,
+        from: data.from,
+        to: data.to,
+        status: data.status,
+        attachment: uploadUrl.data.publicUrl,
+        exp_date: data.incurredDate
+      })
+
+    } catch (error) {
+
+      console.log(error)
+    }
+
   }
 
   return <>
-    <div className="mb-2 flex items-center">
+    <div className="flex items-center mb-2">
       <Link to="/">
-        <Button size='sm' variant='ghost'><ArrowLeftIcon className="h-5 w-5" /></Button>
+        <Button size='sm' variant='ghost'><ArrowLeftIcon className="w-5 h-5" /></Button>
       </Link>
       <h3 className="text-xl">Fill in your expenses</h3>
     </div>
@@ -71,7 +120,7 @@ export const CreateExpensePage = () => {
                       ) : (
                         <span>Pick a date</span>
                       )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      <CalendarIcon className="w-4 h-4 ml-auto opacity-50" />
                     </Button>
                   </FormControl>
                 </PopoverTrigger>
---
+++ supabase/migrations/20241019092415_remote_schema.sql
@@ -0,0 +1,62 @@
+create table "public"."expenses" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone default now(),
+    "exp_date" timestamp without time zone,
+    "from" text,
+    "to" text,
+    "amount" numeric,
+    "status" text,
+    "attachment" text
+);
+
+
+alter table "public"."expenses" enable row level security;
+
+CREATE UNIQUE INDEX expenses_pkey ON public.expenses USING btree (id);
+
+alter table "public"."expenses" add constraint "expenses_pkey" PRIMARY KEY using index "expenses_pkey";
+
+grant delete on table "public"."expenses" to "anon";
+
+grant insert on table "public"."expenses" to "anon";
+
+grant references on table "public"."expenses" to "anon";
+
+grant select on table "public"."expenses" to "anon";
+
+grant trigger on table "public"."expenses" to "anon";
+
+grant truncate on table "public"."expenses" to "anon";
+
+grant update on table "public"."expenses" to "anon";
+
+grant delete on table "public"."expenses" to "authenticated";
+
+grant insert on table "public"."expenses" to "authenticated";
+
+grant references on table "public"."expenses" to "authenticated";
+
+grant select on table "public"."expenses" to "authenticated";
+
+grant trigger on table "public"."expenses" to "authenticated";
+
+grant truncate on table "public"."expenses" to "authenticated";
+
+grant update on table "public"."expenses" to "authenticated";
+
+grant delete on table "public"."expenses" to "service_role";
+
+grant insert on table "public"."expenses" to "service_role";
+
+grant references on table "public"."expenses" to "service_role";
+
+grant select on table "public"."expenses" to "service_role";
+
+grant trigger on table "public"."expenses" to "service_role";
+
+grant truncate on table "public"."expenses" to "service_role";
+
+grant update on table "public"."expenses" to "service_role";
+
+
